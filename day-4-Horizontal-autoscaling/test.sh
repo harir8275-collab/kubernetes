3fb8037c7fde002bd45057a5d43d72a026b3881d
harir8275-collab <harir8275@gmail.com>
2026-05-11 12:55:16 +0530

    Update test.sh

--- day-4-Horizontal-autoscaling/test.sh
+++ day-4-Horizontal-autoscaling/test.sh
@@ -1,6 +1,6 @@
 for j in {1..10}; do  
     for i in {1..1000}; do  
-        curl -s -o /dev/null -w "%{http_code}\n" http://a3d36bd399d15400f8e03cbaaa3f46c9-163245138.us-east-1.elb.amazonaws.com/ &  
+        curl -s -o /dev/null -w "%{http_code}\n" http://a75c0fdfd5ab643c18492b0d077661cd-526350002.us-east-1.elb.amazonaws.com/ &  
     done  
     wait  # Wait for all background curl processes to finish before next iteration
 done
